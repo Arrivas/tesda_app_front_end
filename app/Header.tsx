9635323d2fa70b4e9d8db66f392bef037d71d483
unknown <Mavis@Android-UQFNMK3.Android-UQFNMK3>
2023-03-15 22:09:44 +0800

    merge

---
+++ app/Header.tsx
@@ -0,0 +1,75 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { Bars2Icon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  const navContents = [
+    {
+      id: 1,
+      title: "Home",
+      destination: "/",
+    },
+    {
+      id: 2,
+      title: "Register Borrower",
+      destination: "/rborrower",
+    },
+    {
+      id: 3,
+      title: "Records/Lists",
+      destination: "/",
+    },
+    {
+      id: 4,
+      title: "Inventory",
+      destination: "/",
+    },
+    {
+      id: 5,
+      title: "Logout",
+      destination: "/",
+    },
+  ];
+
+  return (
+    <header
+      className={`p-5 bg-white flex-col items-center justify-center ${
+        showNav ? "" : ""
+      }`}
+    >
+      <nav className="flex flex-row justify-between items-center">
+        <Link href={"/"}>
+          <div className="relative h-14 w-14 cursor-pointer">
+            <Image
+              alt="logo"
+              src="tesda.svg"
+              fill
+              className="object-contain"
+              priority
+            />
+          </div>
+        </Link>
+        <button onClick={() => setShowNav(!showNav)}>
+          <Bars2Icon className="h-6 w-6" />
+        </button>
+      </nav>
+      {showNav && (
+        <nav className="py-2">
+          {navContents.map((item) => (
+            <Link href={item.destination} key={item.id}>
+              <p className="px-2 py-5 hover:bg-[#22396b] rounded-md hover:text-white">
+                {item.title}
+              </p>
+            </Link>
+          ))}
+        </nav>
+      )}
+    </header>
+  );
+};
+
+export default Header;
