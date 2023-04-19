a4e25c8fbaf8247db1f88606ff1c2dbb4fc7139e
unknown <Mavis@Android-UQFNMK3.Android-UQFNMK3>
2023-04-19 17:58:23 +0800

    added filter on print

--- components/charts/BorrowChart.jsx
+++ components/charts/BorrowChart.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   BarChart,
   Bar,
@@ -23,6 +23,35 @@ const activeBorrowItems = [
 const BorrowChart = ({ borrowStats, setBorrowStats }) => {
   const [activeBorrow, setActiveBorrow] = useState(activeBorrowItems[0]);
   const [yearSelect, setYearSelect] = useState(new Date());
+  const [checkFilter, setCheckFilter] = useState("");
+  const [checkItems, setCheckItems] = useState([
+    { id: 1, label: "Borrows", value: "borrows", checked: false },
+    { id: 2, label: "Returns", value: "returns", checked: false },
+  ]);
+
+  useEffect(() => {
+    setCheckItems(
+      activeBorrow.value === "total"
+        ? [
+            { id: 1, label: "Borrows", value: "borrows", checked: false },
+            { id: 2, label: "Returns", value: "returns", checked: false },
+          ]
+        : [
+            {
+              id: 1,
+              label: "Serviceable",
+              value: "serviceable",
+              checked: false,
+            },
+            {
+              id: 2,
+              label: "Unserviceable",
+              value: "unserviceable",
+              checked: false,
+            },
+          ]
+    );
+  }, [activeBorrow]);
 
   const handleSetYear = async (date) => {
     setYearSelect(date);
@@ -34,8 +63,34 @@ const BorrowChart = ({ borrowStats, setBorrowStats }) => {
       .then((res) => setBorrowStats(res.data))
       .catch((err) => console.log(err));
   };
-
   const handleOnBarClick = (data) => {
+    const filteredData = checkFilter
+      ? data.activePayload[0].payload.objects.filter((item) => {
+          if (activeBorrow.value === "total") {
+            if (checkFilter === "borrows" && item.return === "borrowed") {
+              return item;
+            } else if (
+              checkFilter === "returns" &&
+              item.return === "returned"
+            ) {
+              return item;
+            }
+          } else {
+            if (
+              checkFilter === "serviceable" &&
+              item.condition === "Serviceable"
+            ) {
+              return item;
+            } else if (
+              checkFilter === "unserviceable" &&
+              item.condition === "Unserviceable"
+            ) {
+              return item;
+            }
+          }
+        })
+      : data.activePayload[0].payload.objects;
+
     if (data && data.activePayload && data.activePayload.length > 0) {
       const printWindow = window.open("", "Print Table");
       printWindow.document.write(`
@@ -73,11 +128,11 @@ const BorrowChart = ({ borrowStats, setBorrowStats }) => {
                 </tr>
               </thead>
               <tbody>
-                ${data?.activePayload[0].payload?.objects
+                ${filteredData
                   .map(
                     (item) =>
                       `<tr>
-                        <td>${item.propertyNo}</td>  
+                        <td>${item.propertyNo}</td>
                         <td>${item.equipment}</td>
                         <td>${item.borrowerName}</td>
                         <td>${item.condition}</td>
@@ -96,11 +151,21 @@ const BorrowChart = ({ borrowStats, setBorrowStats }) => {
     }
   };
 
+  const handlecheckChange = (onChangeItem) => {
+    const index = checkItems.indexOf(onChangeItem);
+    const newCheckItems = [...checkItems];
+    newCheckItems[index].checked = !newCheckItems[index].checked;
+    setCheckItems(newCheckItems);
+    setCheckFilter(
+      newCheckItems[index].checked ? newCheckItems[index].value : ""
+    );
+  };
+
   return (
     <>
       <div className="flex justify-between items-center gap-2">
         <h1 className="text-2xl font-semibold flex-1">Borrow</h1>
-        <div className="grid grid-cols-2 gap-1">
+        <div className="grid grid-cols-3 gap-1">
           <div className="col-span-1">
             <SelectForm
               type="simple"
@@ -117,6 +182,22 @@ const BorrowChart = ({ borrowStats, setBorrowStats }) => {
               startDate={yearSelect}
               setStartDate={handleSetYear}
             />
+          </div>
+          <div className="col-span-1 items-start justify-center flex flex-col ml-1">
+            <span className="font-semibold bg-white text-xs w-full">
+              select
+            </span>
+            {checkItems.map((item) => (
+              <div key={item.id} className="flex flex-row items-center">
+                <input
+                  type="checkbox"
+                  name={item.value}
+                  onChange={() => handlecheckChange(item)}
+                  checked={item.checked}
+                />
+                <p className="ml-1">{item.label}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
